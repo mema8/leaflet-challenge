c344fbb2beb184ed82a66f86b765a39ab43c1bd2
mema8 <mansoor.mays@gmail.com>
2021-04-01 22:23:08 -0700

    last

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,3 @@
 // API key
 const API_KEY = "pk.eyJ1IjoibWF5c21hbnNvb3IiLCJhIjoiY2ttanIwdHlpMHR5YTJ2dDRqM24yMDJ1OCJ9.31qqoKdyD-7D3RaXZgjqXQ";
+
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,68 +1,68 @@
 
-var titleLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var tileLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox/streets-v10",
+  id: "mapbox/streets-v11",
   accessToken: API_KEY
 })
 
-var myMap = L.map("mapid", {
+var MMap = L.map("mapid", {
   center: [37.09, -95.71],
   zoom: 4
 });
-titleLayer.addTo(myMap);
+tileLayer.addTo(MMap);
 // (Retrieve the data )
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(response) {
-  function styleData(feature) {
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(AllWeekData) {
+  function styles(feature) {
     return {
       color: "#e5e5e5",
       weight: 1,
       fillOpacity: 0.8,
       opacity: 1,
-      fillColor: colors(feature.geometry.coordinates[3]), 
-      radius: radfunc(feature.properties.mag),
+      fillColor: createColors(feature.geometry.coordinates[2]), 
+      radius: createRadius(feature.properties.mag),
       stroke: true
     };
   }
-  function color(magnitudee) {
+  function createColors(colorMag) {
     switch (true) {
-    case magnitudee > 80:
+    case colorMag > 80:
       return "#ebfb9c";
-    case magnitudee > 60: 
+    case colorMag > 60: 
       return "#bcb40a";
-    case magnitudee > 50:
+    case colorMag > 50:
       return "#e0b757";
-    case magnitudee > 35:
+    case colorMag > 35:
       return "#e3965a";
-    case magnitudee > 9: 
+    case colorMag > 9: 
       return "#e5755d";
     default: 
       return "#ebfb9c";
     }
   }
-  function radfunc(rMagnitudee) {
-    if (rMagnitudee === 0) {
+  function createRadius(radiusMag) {
+    if (radiusMag === 0) {
       return 1; 
     }
-    return rMagnitudee * 6;
+    return radiusMag * 6;
   }
-  L.geoJson(response, {
-    pointToLayer: function(feature, latlng) {
-      return L.circleMarker(latlng);   
+  L.geoJson(AllWeekData, {
+    pointToLayer: function(feature, latlon) {
+      return L.circleMarker(latlon);   
   },
-  style: styleData,
+  style: styles,
         onEachFeature: function(feature, layer) {
             layer.bindPopup("Magnitude: "+ feature.properties.mag + "Depth: "+  feature.geometry.coordinates[2]  + "<h3>" + feature.properties.place +
         "</h3>" + new Date(feature.properties.time));
         }
-}).addTo(myMap);
+}).addTo(MMap);
 // Set up the legend
 var legend = L.control({ position: "bottomright" });
 legend.onAdd = function() {
   var div = L.DomUtil.create("div", "info legend");
-  var limits = [-10, 10, 30, 50, 70, 90];
+  var ranges = [-10, 10, 30, 50, 70, 90];
   var color = [
     "#ebfb9c",
     "#bcb40a",
@@ -71,12 +71,12 @@ legend.onAdd = function() {
     "#e5755d",
     "#d9555e"
   ];
-  for (var i = 0; i < limits.length; i++) {
+  for (var i = 0; i < ranges.length; i++) {
     div.innerHTML += "<i style='background:"  + color[i] + "'></i>"
-    + limits[i] + (limits[i + 1] ? "&ndash;" + limits[i + 1] + "<br>" : "+");
+    + ranges[i] + (ranges[i + 1] ? "&ndash;" + ranges[i + 1] + "<br>" : "+");
   }
   return div;
 };
 // Adding legend to the map
-legend.addTo(myMap);
+legend.addTo(MMap);
 });
